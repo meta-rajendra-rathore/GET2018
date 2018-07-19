d2557e51108090fb298e2b1bc58d0831b8c21271
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-07-19 13:02:47 +0530

    Update Shopping.java

--- PF/PF-Assignment-2/Shopping.java
+++ PF/PF-Assignment-2/Shopping.java
@@ -1,36 +1,36 @@
 import java.util.*;
-class Item{													// Item class to construct product detail structure
+class Item {													// Item class to construct product detail structure
 	String name;
 	int quantity;
 	double price;
 	
-	public Item(String name, int quantity, double price){    // @params Product name, quantity and price
-		this.name=name;
-		this.quantity=quantity;
-		this.price=price;
+	public Item(String name, int quantity, double price) {    // @params Product name, quantity and price
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
 	}
 	
-	public String getName(){								// getter-setters
+	public String getName() {								// getter-setters
 		return name;
 	}
 	
-	public int getQuantity(){
+	public int getQuantity() {
 		return quantity;
 	}
 	
-	public double getPrice(){
+	public double getPrice() {
 		return price;
 	}
 	
-	public void setQuantity(int quantity){
+	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
 }
 
-class ShoppingCart{											// ShoppingCart class to contain items
-	HashMap<String,Item> cart = new HashMap<String,Item>();
+class ShoppingCart {											// ShoppingCart class to contain items
+	HashMap <String, Item> cart = new HashMap <String, Item>();
 	
-	public void addItem(){									
+	public void addItem() {									
 		String name;
 		int quantity;
 		double price;
@@ -42,11 +42,11 @@ class ShoppingCart{											// ShoppingCart class to contain items
 		System.out.print("Enter quantity : ");
 		quantity = Integer.parseInt(System.console().readLine());
 		
-		Item newItem = new Item(name,quantity,price);
-		cart.put(name,newItem);
+		Item newItem = new Item(name, quantity, price);
+		cart.put(name, newItem);
 	}
 	
-	public void removeItem(){
+	public void removeItem() {
 		String name;
 		System.out.print("Enter item's name to be removed : ");		// Inquiring which element is to be deleted 
 		name = System.console().readLine();
@@ -58,7 +58,7 @@ class ShoppingCart{											// ShoppingCart class to contain items
 			System.out.println("Item not found!!!");
 	}
 	
-	public void updateItem(){
+	public void updateItem() {
 		String name;
 		int quantity;
 		double price;
@@ -74,9 +74,9 @@ class ShoppingCart{											// ShoppingCart class to contain items
 			System.out.println("Item not found!!!");
 	}
 	
-	public void showItems(){
+	public void showItems() {
 		System.out.println("\nItem Details\n");
-		for(Object obj : cart.values()){							// traversing the hashmap
+		for (Object obj : cart.values()) {							// traversing the hashmap
 			Item item = Item.class.cast(obj);
 			System.out.println("Name : " +  item.getName());
 			System.out.println("Price : " +  item.getPrice());
@@ -84,7 +84,7 @@ class ShoppingCart{											// ShoppingCart class to contain items
 		}
 	}
 	
-	public void showBill(FixedOrderPromotion fixedOrderPromotion, FixedProductPromotion fixedProductPromotion){
+	public void showBill(FixedOrderPromotion fixedOrderPromotion, FixedProductPromotion fixedProductPromotion) {
 		String userCode;
 		Scanner scan = new Scanner(System.in);
 		double totalBill=0.0;
@@ -92,7 +92,7 @@ class ShoppingCart{											// ShoppingCart class to contain items
 		int totalQuantity=0;
 		
 		System.out.println("\n**************\n  Total bill\n**************");
-		for(Object obj : cart.values()){
+		for (Object obj : cart.values()) {
 			Item item = Item.class.cast(obj);
 			System.out.println(item.getName() + " : " + item.getQuantity() + "x" + item.getPrice());
 			totalBill += item.getPrice()*item.getQuantity();		// finding total no of product and total amount in the bill
@@ -102,11 +102,11 @@ class ShoppingCart{											// ShoppingCart class to contain items
 		System.out.println("Total amount : " + totalBill);
 		System.out.print("Enter Promo code (Enter no if you don't have any) : ");
 		userCode = scan.next();
-		if(userCode.equals("no")||userCode.equals("NO"))
+		if (userCode.equals("no") || userCode.equals("NO"))
 			return;
-		if(fixedOrderPromotion.isPromotionApplicable(totalBill,userCode))		// checking order amount based discount
+		if (fixedOrderPromotion.isPromotionApplicable(totalBill,userCode))		// checking order amount based discount
 			totalDiscount = fixedOrderPromotion.getFixedDiscount()*totalBill;
-		else if(fixedProductPromotion.isPromotionApplicable(totalQuantity,totalBill,userCode))	// checking no of product & amount based discount
+		else if (fixedProductPromotion.isPromotionApplicable(totalQuantity,totalBill,userCode))	// checking no of product & amount based discount
 				totalDiscount = fixedProductPromotion.getFixedDiscount()*totalBill;
 			
 		System.out.println("Total discount applicable is : " + totalDiscount);
@@ -114,14 +114,14 @@ class ShoppingCart{											// ShoppingCart class to contain items
 	}
 }
 
-public class Shopping{	
-	public static void main(String[] args){
+public class Shopping {	
+	public static void main(String[] args) {
 		ShoppingCart cart = new ShoppingCart();
 		FixedOrderPromotion fixedOrderPromotion = new FixedOrderPromotion(5000.00);				// set the limits for discounts
 		FixedProductPromotion fixedProductPromotion = new FixedProductPromotion(20,2000.00);	// on the two different bases
 		char choice;
 		Scanner scan = new Scanner(System.in);
-		while(true){
+		while (true) {
 			System.out.println("\nShopping Cart menu\n");
 			System.out.println("1.Add an item");
 			System.out.println("2.Remove an item");
@@ -132,7 +132,7 @@ public class Shopping{
 			System.out.print("Enter Your choice : ");
 			choice=scan.next().charAt(0);
 			
-			switch(choice){
+			switch (choice) {
 				case '1' : 
 					cart.addItem();
 					break;
