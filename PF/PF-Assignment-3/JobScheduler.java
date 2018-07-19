90c8d44d51b123603d0c593d69416a566ff35a8f
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-07-19 17:47:23 +0530

    Update JobScheduler.java
    Removed repeated loops and implemented reusability.

--- PF/PF-Assignment-3/JobScheduler.java
+++ PF/PF-Assignment-3/JobScheduler.java
@@ -11,7 +11,7 @@ public class JobScheduler {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter number of processes : ");	// Asking no of processes to schedule
 		int noOfProcess = scan.nextInt();
-		if(noOfProcess == 0) {
+		if (noOfProcess == 0) {
 			System.out.println("There are no jobs to schedule."); 
 			System.exit(0);									// terminate program because no processes to schedule
 		}
@@ -25,34 +25,24 @@ public class JobScheduler {
 		int maximumWaitingTime = 0;
 		int maximumWaitingTimeIndex = 0;
 
-		System.out.println("\nARRIVAL TIME");		// taking arrival time
-		for(int i = 0 ; i < noOfProcess ; i++) {
-			System.out.print("Enter arrival time of process " + (i+1) + " : ");
+		for (int i = 0 ; i < noOfProcess ; i++) {	// taking arrival time and burst time
+			
+			System.out.print("\nEnter arrival time of process " + (i+1) + " : ");
 			arrivalTime[i] = scan.nextInt();
-		}
-
-		System.out.println("\nBURST TIME");			// taking burst time
-		for(int i = 0 ; i < noOfProcess ; i++) {
 			System.out.print("Enter burst time of process " + (i+1) + " : ");
 			burstTime[i] = scan.nextInt();
-		}
-		
-		for (int i = 0; i < noOfProcess; i++) {
+			
 			if ((arrivalTime[i] < 0) || (burstTime[i] < 0)) {
 				System.out.println("Arrival Time or Burst Time can't be negative");
 			}
-		}
-		
-		for (int i = 0; i < noOfProcess; i++) {
-			if ((i + 1) < noOfProcess) {
-				if (arrivalTime[i] >= arrivalTime[i + 1]) {
-					System.out.println("Arrival Time of Process " + (i + 1) + " is greater than or equal to " + (i + 2));
+			
+			if ((i + 1) <= noOfProcess && i != 0) {
+				if (arrivalTime[i] < arrivalTime[i - 1]) {
+					System.out.println("Arrival Time of Process " + i + " is earlier than earlier process "");
 					System.exit(1);
 				}
 			}
-		}
-		
-		for (int i = 0; i < noOfProcess; i++) {		
+			
 			if (i==0) {
 				completionTime[i] = arrivalTime[i] + burstTime[i];
 			} else {
@@ -67,15 +57,16 @@ public class JobScheduler {
 			totalWaitingTime += waitingTime[i];
 		}
 		
-		System.out.println("Process No\tCompletion Time\tTurnAround Time\tWaiting Time");
+		System.out.println("\n\nProcess No\tCompletion Time\tTurnAround Time\tWaiting Time");
+							   
 		for (int i = 0; i < noOfProcess; i++) {
 			System.out.println(i + 1 + "\t\t" +completionTime[i] + "\t\t\t" + turnAroundTime[i] + "\t\t" + waitingTime[i]);
-			if(maximumWaitingTime < waitingTime[i]) {
+			if (maximumWaitingTime < waitingTime[i]) {
 				maximumWaitingTime = waitingTime[i];
 			}
 		}
 		
-		System.out.println("Maximum Waiting Time :- " + maximumWaitingTime);
+		System.out.println("\n\nMaximum Waiting Time : " + maximumWaitingTime);
 		avgWaitingTime = totalWaitingTime / noOfProcess;
 		System.out.println("The average waiting time is " + avgWaitingTime);
 	}
