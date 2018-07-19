a16b714295ed7ee9ff55b30c58c7828c9c5b8397
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-07-19 12:56:41 +0530

    Update FixedProductPromotion.java

--- PF/PF-Assignment-2/FixedProductPromotion.java
+++ PF/PF-Assignment-2/FixedProductPromotion.java
@@ -1,73 +1,95 @@
+/*
+ * This file introduces an interface promotion and also provide promotion on the basic of products
+ * @authore Rajendra Singh Rathore
+ *
+ */
+
 import java.util.Calendar;
 
-interface Promotion{
-	enum PromotionEnum{
-		get5("GET05",1,5),free5("FREE05",15,20),less5("LESS05",25,30);  // @params promotion code,start and end date of in a month 
+interface Promotion {
+	enum PromotionEnum {
+		get5("GET05", 1, 5), free5("FREE05", 15, 20), less5("LESS05", 25, 30);  // @params promotion code,start and end date of in a month 
 		String promoCode;
 		double discount;
 		int startDate,endDate;
 		
-		private PromotionEnum(String code,int start,int end){	// enum constructor
+		private PromotionEnum(String code, int start, int end) {	// enum constructor
 			promoCode = code;
 			startDate = start;
-			endDate = end;
+			endDate	= end;
 		}
 		
-		public String getPromoCode(){
+		public String getPromoCode() {
 			return promoCode;
 		}
 		
-		public int getStartDate(){
+		public int getStartDate() {
 			return startDate;
 		}
 		
-		public int getEndDate(){
+		public int getEndDate() {
 			return endDate;
 		}
 	}
+	
+	public FixedProductPromotion(int minProducts, double minBill);
+	
+	public double getMinQuantity();
+	
+	public double getMinimumPrice();
+	
+	public void setMinimumPrice(int quantity, double price);
+	
+	public double getFixedDiscount();
+	
+	public void setFixedDiscount();
+	
+	public boolean isPromotionApplicable(int productInBill, double billAmount, String userCode);
+	}
 }
 
-public class FixedProductPromotion implements Promotion{		// public class implementing interface Promotion
+public class FixedProductPromotion implements Promotion {		// public class implementing interface Promotion
 	int minQuantity;
 	double minimumPrice;
 	double fixedDiscount;
 	
-	public FixedProductPromotion(int minProducts, double minBill){
+	public FixedProductPromotion(int minProducts, double minBill) {
 		setMinimumPrice(minProducts,minBill);					// setting up minimum price and no of products limit to avail discount
 		setFixedDiscount();										// also declaring discount percent
 	}
 	
-	public double getMinQuantity(){
+	public double getMinQuantity() {
 		return minQuantity;
 	}
 	
-	public double getMinimumPrice(){
+	public double getMinimumPrice() {
 		return minimumPrice;
 	}
 	
-	public void setMinimumPrice(int quantity, double price){
+	public void setMinimumPrice(int quantity, double price) {
 		minQuantity = quantity;
 		minimumPrice = price;
 	}
 	
-	public double getFixedDiscount(){
+	public double getFixedDiscount() {
 		return fixedDiscount;
 	}
 	
-	public void setFixedDiscount(){
+	public void setFixedDiscount() {
 		fixedDiscount = 0.05;	// 5% discount is given 
 	}
 	
-	public boolean isPromotionApplicable(int productInBill,double billAmount, String userCode){ // @params no of products in bill, total amount, promo code entered
+	public boolean isPromotionApplicable(int productInBill, double billAmount, String userCode) { // @params no of products in bill, total amount, promo code entered
 		Calendar cal = Calendar.getInstance();													// @return boolean variable to show discount availability
 		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-		if(billAmount>=getMinimumPrice())
-			if(productInBill>=getMinQuantity()){
-				for(PromotionEnum promo : PromotionEnum.values())
-					if(userCode.equals(promo.getPromoCode())){
-						if(dayOfMonth>=promo.getStartDate()&&dayOfMonth<=promo.getEndDate())
+		
+		if (billAmount >= getMinimumPrice())
+			if(productInBill >= getMinQuantity()) {
+				for (PromotionEnum promo : PromotionEnum.values())
+					if (userCode.equals(promo.getPromoCode())) {
+						if (dayOfMonth >= promo.getStartDate() && dayOfMonth <= promo.getEndDate())
 							return true;
-						else{
+						else {
 							System.out.println("SORRY!!! This promo-code is not active now.");
 							return false;
 						}
