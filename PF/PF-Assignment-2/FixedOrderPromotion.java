e7822a9faa943aa9f252bc73204a755c152bd3cb
Rathore <rathore1729@gmail.com>
2018-07-23 19:54:30 +0530

    Updating branch name

--- PF/PF-Assignment-2/FixedOrderPromotion.java
+++ PF/PF-Assignment-2/FixedOrderPromotion.java
@@ -1,39 +1,48 @@
-import java.util.Calendar;
+package shopping;
 
-public class FixedOrderPromotion implements Promotion{		// class implementing interface Promotion
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class FixedOrderPromotion implements Promotion {		// class implementing interface Promotion
 	double minimumPrice;
 	double fixedDiscount;
 	
-	public FixedOrderPromotion(double minBill){
+	public FixedOrderPromotion(double minBill, int discount) {
 		setMinimumPrice(minBill);					// setting up minimum price limit to avail discount
-		setFixedDiscount();							// also declaring discount percent
+		setFixedDiscount(discount);							// also declaring discount percent
 	}
 	
-	public double getMinimumPrice(){
+	public double getMinQuantity() {  
+		return 0;
+	}
+	
+	public double getMinimumPrice() {
 		return minimumPrice;
 	}
 	
-	public void setMinimumPrice(double price){
+	public void setMinimumPrice(double price) {
 		minimumPrice = price;
 	}
 	
-	public double getFixedDiscount(){
+	public double getFixedDiscount() {
 		return fixedDiscount;
 	}
 	
-	public void setFixedDiscount(){
-		fixedDiscount = 0.05;						// 5% discount is given 
+	public void setFixedDiscount(int discount) {
+		fixedDiscount = discount;						// 5% discount is given 
 	}
 	
-	public boolean isPromotionApplicable(double billAmount, String userCode){ 			// @params total amount, promo code entered
-		if(billAmount>=getMinimumPrice()){												// @return boolean variable to show discount availability
-		Calendar cal = Calendar.getInstance();
-		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-			for(PromotionEnum promo : PromotionEnum.values())
-				if(userCode.equals(promo.getPromoCode())){
-					if(dayOfMonth>=promo.getStartDate()&&dayOfMonth<=promo.getEndDate())
+	public boolean isPromotionApplicable(int products, double billAmount, String userCode) { 			// @params total amount, promo code entered
+		if (billAmount >= getMinimumPrice()) {												// @return boolean variable to show discount availability
+			Calendar cal = Calendar.getInstance();
+			final Date date = cal.getTime();
+			String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(date);		// @return boolean variable to show discount availability
+				for (PromotionEnum promo : PromotionEnum.values())
+				if (userCode.equals(promo.getPromoCode())) {
+					if(currentDate.compareTo(promo.getStartDate())>=0 && currentDate.compareTo(promo.getEndDate())<=0) {
 						return true;
-					else{
+					} else {
 						System.out.println("SORRY!!! This promo-code is not active now.");
 						return false;
 					}
--- PF/PF-Assignment-2/FixedProductPromotion.java
+++ PF/PF-Assignment-2/FixedProductPromotion.java
@@ -1,81 +1,53 @@
+package shopping;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
-interface Promotion{
-	enum PromotionEnum{
-		get5("GET05",1,5),free5("FREE05",15,20),less5("LESS05",25,30);  // @params promotion code,start and end date of in a month 
-		String promoCode;
-		double discount;
-		int startDate,endDate;
-		
-		private PromotionEnum(String code,int start,int end){	// enum constructor
-			promoCode = code;
-			startDate = start;
-			endDate = end;
-		}
-		
-		public String getPromoCode(){
-			return promoCode;
-		}
-		
-		public int getStartDate(){
-			return startDate;
-		}
-		
-		public int getEndDate(){
-			return endDate;
-		}
-	}
-}
-
-public class FixedProductPromotion implements Promotion{		// public class implementing interface Promotion
-	int minQuantity;
+public class FixedProductPromotion implements Promotion {		// public class implementing interface Promotion
 	double minimumPrice;
 	double fixedDiscount;
 	
-	public FixedProductPromotion(int minProducts, double minBill){
-		setMinimumPrice(minProducts,minBill);					// setting up minimum price and no of products limit to avail discount
-		setFixedDiscount();										// also declaring discount percent
+	public FixedProductPromotion(double minBill, int discount) {
+		setMinimumPrice(minBill);					// setting up minimum price and no of products limit to avail discount
+		setFixedDiscount(discount);										// also declaring discount percent
 	}
 	
-	public double getMinQuantity(){
-		return minQuantity;
-	}
-	
-	public double getMinimumPrice(){
+	public double getMinimumPrice() {
 		return minimumPrice;
 	}
 	
-	public void setMinimumPrice(int quantity, double price){
-		minQuantity = quantity;
+	public void setMinimumPrice(double price) {
 		minimumPrice = price;
 	}
 	
-	public double getFixedDiscount(){
+	public double getFixedDiscount() {
 		return fixedDiscount;
 	}
 	
-	public void setFixedDiscount(){
-		fixedDiscount = 0.05;	// 5% discount is given 
+	public void setFixedDiscount(int discount) {
+		fixedDiscount = discount;
 	}
 	
-	public boolean isPromotionApplicable(int productInBill,double billAmount, String userCode){ // @params no of products in bill, total amount, promo code entered
-		Calendar cal = Calendar.getInstance();													// @return boolean variable to show discount availability
-		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-		if(billAmount>=getMinimumPrice())
-			if(productInBill>=getMinQuantity()){
-				for(PromotionEnum promo : PromotionEnum.values())
-					if(userCode.equals(promo.getPromoCode())){
-						if(dayOfMonth>=promo.getStartDate()&&dayOfMonth<=promo.getEndDate())
+	public boolean isPromotionApplicable(int productInBill,double billAmount, String userCode) { // @params no of products in bill, total amount, promo code entered
+		Calendar cal = Calendar.getInstance();
+		final Date date = cal.getTime();
+		String currentDate = new SimpleDateFormat("yyyy-MM-dd").format(date);		// @return boolean variable to show discount availability
+		
+		if(billAmount >= getMinimumPrice()) {
+			for(PromotionEnum promo : PromotionEnum.values()) {
+					if(userCode.equals(promo.getPromoCode())) {
+						System.out.println("Date : " + currentDate);
+						if(currentDate.compareTo(promo.getStartDate())>=0 && currentDate.compareTo(promo.getEndDate())<=0) {
 							return true;
-						else{
+						} else {
 							System.out.println("SORRY!!! This promo-code is not active now.");
 							return false;
 						}
 					}
-				System.out.println("SORRY!!! This promo-code doesn't exist.");
 			} 
-			else
-				System.out.println("You don't have the minimum number of products to avail any offers.");
+			System.out.println("SORRY!!! This promo-code doesn't exist.");
+		}
 		else
 			System.out.println("You don't reach minimum bill amount to avail any offers.");
 		return false;
