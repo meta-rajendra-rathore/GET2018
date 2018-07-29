1a85952ff0690f14b99d19447e138079297ada20
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-07-29 12:39:22 +0530

    Update PolyTest.java

--- SCF/SCF-Session-6/PolyTest.java
+++ SCF/SCF-Session-6/PolyTest.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PolyTest {
-    Poly poly1 = new Poly(new int[]{4, 2, 1, 2});
+    Poly poly1 = new Poly(new int[][]{ {0, 2}, {4, 2}, {2, 3}});
 
     @Test
     public void testEvaluate() {
-        assertEquals(28.0, poly1.evaluate(2.0F), 0.001);
+        assertEquals(46.0, poly1.evaluate(2.0F), 0.001);
     }
 
     @Test
@@ -19,17 +19,14 @@ public class PolyTest {
 
     @Test
     public void testAddPoly() {
-        Poly poly2 = new Poly(new int[]{3, 5, 4});
-        assertArrayEquals(new int[]{7, 7, 5, 2}, poly1.addPoly(poly1, poly2));
+        Poly poly2 = new Poly(new int[][]{ {0, 1}, {4, 3}, {2, 2}});
+        assertArrayEquals(new int[][]{ {0, 3}, {4, 5}, {2, 5}}, Poly.addPoly(poly1, poly2)); 
     }
 
     @Test
     public void testMultiplyPoly() {
-        Poly poly2 = new Poly(new int[]{2, 1, 3});
-        assertArrayEquals(new int[]{8, 8, 16, 11, 5, 6}, poly1.multiplyP(poly1, poly2));
-        
-        Poly poly3 = new Poly(new int[]{1, 3});
-        assertArrayEquals(new int[]{4, 14, 7, 5, 6}, poly1.multiplyP(poly1, poly3));
+        Poly poly2 = new Poly(new int[][]{ {0, 2}, {2, 3}});
+        assertArrayEquals(new int[][]{ {0, 4}, {2, 12}, {4, 13}, {6, 6}}, Poly.multiplyPoly(poly1, poly2));
     }
 
 }
