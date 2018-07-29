a600236db9d6e87cfe8de3cfe43ae1b39d7a2828
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-07-29 12:35:23 +0530

    Update IntSetTest.java

--- SCF/SCF-Session-6/IntSetTest.java
+++ SCF/SCF-Session-6/IntSetTest.java
@@ -6,29 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class IntSetTest {
-	IntSet intSet;
-	
-	@Before
-	public void init() {
-		intSet = new IntSet();    // Defining an intSet
-		intSet.addItem(1);        // add few elements to the set
-		intSet.addItem(3);
-		intSet.addItem(4);
-		intSet.addItem(2);
-		intSet.addItem(5);
-		intSet.addItem(8);
-	}
-
-	@Test
-	public void testAddItem() {    // testing addItem function
-		assertEquals(false, intSet.addItem(3));
-	}
-	
-	@Test(expected = AssertionError.class)     // testing error occurance when invalid elements are passed
-	public void testAddItemError() {
-		intSet.addItem(-10);
-		intSet.addItem(20);
-	}
+	IntSet intSet = new IntSet(15, new int[] {2, 3, 5, 6, 9, 12});    // Defining an intSet
 
 	@Test
 	public void testIsMember() {           // test isMember function
@@ -42,59 +20,34 @@ public class IntSetTest {
 	}
 
 	@Test
-	public void testNewSize() {
-		intSet.addItem(9);
-		assertEquals(7, intSet.size());
-	}
-
-	@Test
 	public void testIsSubsetTrue() {
-		IntSet newSet = new IntSet();
-		newSet.addItem(5);
-		newSet.addItem(2);
-		newSet.addItem(8);
+		IntSet newSet = new IntSet(15, new int[] {2, 5, 3, 9});
 		assertEquals(true, intSet.isSubSet(newSet));
 	}
 
 	@Test
 	public void testIsSubsetFalse() {
-		IntSet newSet = new IntSet();
-		newSet.addItem(1);
-		newSet.addItem(5);
-		newSet.addItem(9);
+		IntSet newSet = new IntSet(15, new int[] {2, 3, 4});
 		assertEquals(false, intSet.isSubSet(newSet));
 	}
 
 	@Test
 	public void testGetComplement() {
-		int[] expectedArray1 = {6, 7, 9, 10, 11, 12, 13, 14, 15};
+		int[] expectedArray1 = {1, 4, 7, 8, 10, 11, 13, 14, 15};
 		assertArrayEquals(expectedArray1, intSet.getComplement());
 	}
 	
 	@Test(expected = AssertionError.class)     // test error occurrence if null set is passed 
 	public void testGetComplementError() {
-		intSet.addItem(6);
-		intSet.addItem(7);
-		intSet.addItem(9);
-		intSet.addItem(10);
-		intSet.addItem(11);
-		intSet.addItem(12);
-		intSet.addItem(13);
-		intSet.addItem(14);
-		intSet.addItem(15);
-		intSet.getComplement();
+	    IntSet newSet = new IntSet(15, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15});
+	    assertEquals("Complement of this set is null set.", newSet.getComplement());
 	}
 
 	@Test
 	public void testUnion() {
-		IntSet newSet = new IntSet();
-		newSet.addItem(10);
-		newSet.addItem(3);
-		newSet.addItem(14);
-		newSet.addItem(2);
-		newSet.addItem(15);
-		int[] expectedArray = {1, 3, 4, 2, 5, 8, 10, 14, 15, 0, 0};
-		assertArrayEquals(expectedArray, intSet.union(intSet, newSet));
+		IntSet newSet = new IntSet(15, new int[] {1, 4, 14, 15});
+		int[] expectedArray = {2, 3, 5, 6, 9, 12, 1, 4, 14, 15};
+		assertArrayEquals(expectedArray, IntSet.union(intSet, newSet));
 	}
 
 }
