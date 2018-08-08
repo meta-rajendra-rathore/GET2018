512387f31214c67e189d6f9b4473ab13665e0c1f
Rajendra Singh Rathore <41181622+meta-rajendra-rathore@users.noreply.github.com>
2018-08-08 13:52:21 +0530

    Add files via upload

--- DS/DS-Session-3/virtualCMD/VirtualCMD.java
+++ DS/DS-Session-3/virtualCMD/VirtualCMD.java
@@ -5,9 +5,6 @@
 
 package datastructure_3.virtualCMD;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 import java.util.Scanner;
 
 public class VirtualCMD {
@@ -146,51 +143,4 @@ public class VirtualCMD {
         }
         return result;
     }
-}
-
-class Directory {
-    private String name;
-    private Directory parent;
-    private String timeStamp;
-    private List<Directory> listOfSubDirectories;
-
-    public Directory(String name, Directory parent) {
-        this.name = name;
-        this.parent = parent;
-        timeStamp = Calendar.getInstance().getTime().toString();
-        listOfSubDirectories = new ArrayList<Directory>();
-    }
-
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @return the parent
-     */
-    public Directory getParent() {
-        return parent;
-    }
-
-    /**
-     * @return the timeStamp
-     */
-    public String getTimeStamp() {
-        return timeStamp;
-    }
-
-    /**
-     * @return the listOfSubDirectories
-     */
-    public List<Directory> getListOfSubDirectories() {
-        return listOfSubDirectories;
-    }
-
-    public void addItem(Directory item) {
-        listOfSubDirectories.add(item);
-    }
-
 }
