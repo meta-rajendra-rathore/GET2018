1e46c8f7919930be7c00ed9cceafb31c08112690
Rajendra Rathore <rajendra.rathore@metacube.com>
2018-08-21 12:11:31 +0530

    indices

--- DBMS/DBMS-Session-4/assignment_procedures.sql
+++ DBMS/DBMS-Session-4/assignment_procedures.sql
@@ -4,31 +4,59 @@ Month and year will be input parameter to function. */
 
 USE storefront;
 
+DROP PROCEDURE IF EXISTS avg_sales_in_month;
+
 DELIMITER $$
 CREATE PROCEDURE avg_sales_in_month (
     IN query_month INT,
-    IN query_year INT,
-    OUT avg_sales DECIMAL )
+    IN query_year INT )
 BEGIN
-    SELECT TRUNCATE(SUM(product_quantity)/COUNT(product_ID), 2) INTO avg_sales FROM Products_In_Order GROUP BY PRODUCT_ID;
+    SELECT 
+        product_ID AS ID, 
+        product.Title, 
+        TRUNCATE(SUM(product_quantity)/COUNT(product_ID), 2) AS 'Average Sales' 
+    FROM Products_In_Order 
+    LEFT JOIN
+    Product
+    ON ID = product_ID
+    GROUP BY PRODUCT_ID;
 END $$
 
-CALL avg_sales_in_month(8, 2018, @avg_sales);
-SELECT @avg_sales;
+CALL avg_sales_in_month(8, 2018);
     
 /* 2.
 Create a stored procedure to retrieve table having order detail with status for a given period. 
 Start date and end date will be input parameter. Put validation on input dates like start date is less than end date. 
 If start date is greater than end date take first date of month as start date. */
 
-DELIMITER $$
-CREATE PROCEDURE avg_sales (
-    IN 
-    IN 
-    OUT 
-    OUT 
-    
-)
-BEGIN
+DROP PROCEDURE IF EXISTS order_details_in_period;
 
-END $$
+DELIMITER ^^
+CREATE PROCEDURE order_details_in_period (
+    IN start_date DATE,
+    IN end_date DATE )
+BEGIN
+    SELECT 
+        Orders.ID AS 'Order ID',
+        Product.ID AS 'Product ID',
+        Product.title AS 'Product Name',
+        Orders.order_date AS 'Order date',
+        Products_In_Order.order_status AS 'Order status'
+    FROM Orders
+    LEFT JOIN
+    Products_In_Order
+    ON Orders.ID = Products_In_Order.order_ID
+    LEFT JOIN
+    Product
+    ON Products_In_Order.product_ID = Product.ID
+    WHERE order_date 
+        BETWEEN IF(start_date >= end_date, DATE_FORMAT(start_date, "%Y-%m-01"), start_date) 
+        AND end_date
+    ORDER BY order_date;
+END ^^
+
+CALL order_details_in_period( STR_TO_DATE("2018-08-05", "%Y-%m-%d"),STR_TO_DATE("2018-08-10", "%Y-%m-%d") );
+
+CALL order_details_in_period( STR_TO_DATE("2018-08-15", "%Y-%m-%d"),STR_TO_DATE("2018-08-09", "%Y-%m-%d") );
+
+CALL order_details_in_period( STR_TO_DATE("2018-08-07", "%Y-%m-%d"),STR_TO_DATE("2018-08-7", "%Y-%m-%d") );
