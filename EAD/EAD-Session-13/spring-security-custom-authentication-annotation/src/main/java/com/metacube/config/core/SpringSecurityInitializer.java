4c063e1859b55f0289ff359b995bf82586d15561
Rajendra Rathore <rajendra.rathore@metacube.com>
2018-09-15 13:56:14 +0530

    Yet to be completed

---
+++ EAD/EAD-Session-13/spring-security-custom-authentication-annotation/src/main/java/com/metacube/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.metacube.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
